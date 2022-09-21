24eaa3ed023ca5fefc54615210ca020c943461f8
noy2 <gameplay3542@icloud.com>
2022-09-21 20:29:02 +0900

    Modified: Changed file location

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./1-1/App";
+import App from "./2-1/App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
