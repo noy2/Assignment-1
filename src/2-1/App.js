d6ee7a1668c50751df9867463c9d54de344bb5e5
noy2 <gameplay3542@icloud.com>
2022-09-22 06:04:16 +0900

    Modified: 스테이트 끌어올리기, 함수 변경

--- src/2-1/App.js
+++ src/2-1/App.js
@@ -1,10 +1,14 @@
 import Counter from "./Counter"
+import { useState } from "react";
 
 export default function App() {
 
+  const [num, setNum] = useState(0);
+
   return (
     <div className="App">
-      <Counter></Counter>
+      <Counter num={num} setNum={setNum}></Counter>
+      <Counter num={num} setNum={setNum}></Counter>
     </div>
   );
 }
--- src/2-1/Counter.js
+++ src/2-1/Counter.js
@@ -1,17 +1,10 @@
-import { useState } from "react";
+export default function Counter(props) {
 
-export default function Counter() {
-  const [num, setNum] = useState(0);
-
-  const handleChange = (event) => {
-    // console.log(event.target.className);
-    event.target.className === "plus" ? setNum(num+1) : setNum(num-1);
-  }
   return (
     <div>
-      <p>{num}</p>
-      <button className="plus" onClick={handleChange}>+1</button>
-      <button className="minus" onClick={handleChange}>-1</button>
+      <p>{props.num}</p>
+      <button onClick={() => props.setNum(props.num+1)}>+1</button>
+      <button onClick={() => props.setNum(props.num-1)}>-1</button>
     </div>
   );
 }
